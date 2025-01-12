307e33fbab5baf1aa226c24170c4f2510499ae91
Brenda Aceves <brendaaceves70@developforgood.org>
2025-01-11 16:38:45 -0800

    refactor: Simplified and organized component logic by adding comments and removing redundant styling. feat: Added new careerImage property to component CareerCard, which displays the intended career image if the user is in middle/high school

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import CareersCard from './Components/CareersCard/CareersCard.jsx';
+import CareersCard from './Components/CareersCard/CareersCard.js';
 
 function App() {
   return (
   <div className="App">
     <CareersCard 
-      isElementary={true}
+      isElementary={false}
       title={"Agricultural Architect"} 
       headerColor={'green'}
-      description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."}/>
+      description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."}
+      careerImage={'https://placehold.co/250x250'}
+    />
   </div>
   );
 }
