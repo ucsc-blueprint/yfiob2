d93f4735d60f7a280cdb87e24ae3c9e8418d050f
Brenda Aceves <brendaaceves70@developforgood.org>
2025-01-14 18:10:05 -0800

    feat: added dynamic implementation for career card expanded component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { CareersCardExpanded } from "./components/CareersCardExpanded/CareersCardExpanded";
 import { Navbar } from "./components/Navbar";
 
 function App() {
 	return (
 		<div className="bg-[#FFC273] h-[100vh]">
 			<Navbar />
+			<CareersCardExpanded 
+				category={'Agriculture and Natural Resources'} 
+				careerName={'Agricultural Architect'} 
+				description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed'}	
+				salary={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed'}
+				careerImages={['https://placehold.co/350x250', 'https://placehold.co/350x250', 'https://placehold.co/350x250']}
+			/>
 		</div>
 	);
 }
