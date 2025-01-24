8c4eac6244552a40c38f93d13a8f16b7525a3ed2
Brenda Aceves <brendaaceves70@developforgood.org>
2025-01-24 15:32:16 -0800

    feat: implemented styling for elementary-school view

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 			<Navbar />
 			<div className="flex justify-center">
 			<CareersCard 
-				educationLevel={'high-school'}
+				educationLevel={'elementary-school'}
 				title={"Agricultural Architect"} 
 				headerColor={'green'}
 				description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."}
