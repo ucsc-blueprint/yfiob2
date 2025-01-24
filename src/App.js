4f873260beb562f448a68e52ecb0b079e4f78513
Brenda Aceves <brendaaceves70@developforgood.org>
2025-01-24 15:41:18 -0800

    didn't add app.js to last commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 			<Navbar />
 			<div className="flex justify-center">
 			<CareersCard 
-				educationLevel={'middle-school'}
+				educationLevel={'elementary-school'}
 				title={"Agricultural Architect"} 
 				description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."}
 				careerImage={'https://placehold.co/200x200'}
