d48b563bf0fa86061488e475c2b45e8e82548a4b
Brenda Aceves <brendaaceves70@developforgood.org>
2025-01-24 15:40:18 -0800

    modified middle-school/high-school careers card to match styling width/height of elementary-level cards

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
 			<Navbar />
 			<div className="flex justify-center">
 			<CareersCard 
-				educationLevel={'elementary-school'}
+				educationLevel={'middle-school'}
 				title={"Agricultural Architect"} 
 				description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."}
-				careerImage={'https://placehold.co/250x250'}
+				careerImage={'https://placehold.co/200x200'}
 				/>
 			</div>
 		</div>
--- src/Components/CareersCard.jsx
+++ src/Components/CareersCard.jsx
@@ -4,23 +4,23 @@ export default function CareersCard({title, description, educationLevel, careerI
   if (educationLevel === 'elementary-school') {
     // Display elementary-level card summary
     return (
-      <div className="no-scrollbar overflow-y-auto bg-white rounded-tr-xl rounded-b-xl border-black border-2  w-[250px] h-[250px] font-lato ">
-        <header className='p-3 text-center text-[22px] font-bold text-lg pd-[0.7em]'> {title} </header>
+      <div className="no-scrollbar overflow-y-auto bg-white rounded-tr-xl rounded-b-xl border-black border-2  w-[240px] h-[250px] font-lato ">
+        <header className='p-3 text-center text-[20px] font-bold text-lg pd-[0.7em]'> {title} </header>
         <p className='pl-5 pr-5 text-[12px]'> {description} </p>
       </div>
     )
   } else {
     // Otherwise, display middle/high school level card summary
     return (
-      <div className="no-scrollbar overflow-y-auto bg-white rounded-3xl w-[320px] h-[380px] rounded-15px pb-1em font-lato ">
+      <div className="no-scrollbar overflow-y-auto bg-white rounded-3xl w-[240px] h-[250px] rounded-15px pb-1em font-lato ">
         { educationLevel === 'middle-school' ? 
         // Header for middle-school level
-        <header className='p-3 bg-[#3FA1D9] text-center text-[22px] font-bold text-lg pd-[0.7em]'> {title} </header>
+        <header className='pt-2 pb-2 bg-[#3FA1D9] text-center text-[22px] font-bold text-lg pd-[0.7em]'> {title} </header>
         :
         // Header for high-school level
-        <header className="p-3 bg-[#47B748] text-center text-[22px] font-bold text-lg pd-[0.7em]">{title}</header>
+        <header className="pt-2 pb-2 bg-[#47B748] text-center text-[22px] font-bold text-lg pd-[0.7em]">{title}</header>
         }
-        <p className="p-6">{description}</p>
+        <p className="pl-5 pr-5 pt-3 pb-3 text-[12px]">{description}</p>
         <img className='m-auto mb-5' src={careerImage} alt='career-image'></img>
       </div>        
       );
