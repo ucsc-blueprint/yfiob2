f3a4cebf410a616af6e2c3fcc1970588897a89f9
Kartikeya <kartikeyaku@gmail.com>
2025-01-28 18:40:47 -0800

    Finished half of task

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import { Navbar } from "./Components/Navbar";
 import { QuestionCard } from "./Components/QuestionCard";
+import { BeforeYouStart } from "./Components/BeforeYouStart/BeforeYouStart";
 
 function App() {
 	return (
-		<div className="bg-[#FFC273] h-[100vh]">
-			<Navbar />
-			<div className="flex justify-center">
-				<QuestionCard
-					advice={"here's some advice"}
-					question={"test question"}
-					questionNumber={5}
-					totalQuestions={6}
-				/>
-			</div>
-		</div>
+		<>
+		<BeforeYouStart></BeforeYouStart>
+		</>
 	);
 }
 
---
+++ src/Components/BeforeYouStart/BeforeYouStart.jsx
@@ -0,0 +1,18 @@
+import './BeforeYouStart.css'
+
+export const BeforeYouStart = () => {
+    return (
+        <>
+        <div className = "header">Before You Start Answering</div>
+        
+        <div className = "dropContainer">
+            <select className = "dropdown" name = "School" id ="School">
+                <option selected disabled>school</option>
+            </select>
+            <select className = "dropdown" name = "School" id = "Grade">
+                <option selected disabled>grade</option>
+            </select>
+        </div>
+        </>
+    )
+}
