d485eedbb62520a0fa1b2e88613fd1575f9a131e
Kartikeya <kartikeyaku@gmail.com>
2025-01-28 18:58:07 -0800

    Finished 4/5 of task

--- src/Components/BeforeYouStart/BeforeYouStart.jsx
+++ src/Components/BeforeYouStart/BeforeYouStart.jsx
@@ -3,18 +3,22 @@ import { Navbar } from '../Navbar'
 
 export const BeforeYouStart = () => {
     return (
-        <>
-        <Navbar></Navbar>
-        <div className = "header">Before You Start Answering</div>
-        
-        <div className = "dropContainer">
-            <select className = "dropdown" name = "School" id ="School">
-                <option selected disabled>school</option>
-            </select>
-            <select className = "dropdown" name = "School" id = "Grade">
-                <option selected disabled>grade</option>
-            </select>
+        <div
+        // style={{
+        //     backgroundColor: 'blue',
+        //   }}
+        >
+            <Navbar></Navbar>
+            <div className = "header">Before You Start Answering</div>
+            
+            <div className = "dropContainer">
+                <select className = "dropdown" name = "School" id ="School">
+                    <option selected disabled>school</option>
+                </select>
+                <select className = "dropdown" name = "School" id = "Grade">
+                    <option selected disabled>grade</option>
+                </select>
+            </div>
         </div>
-        </>
     )
 }
