685104aadec73fa74c0aebc8256b13501265af01
Kartikeya <kartikeyaku@gmail.com>
2025-01-31 17:48:04 -0800

    Finished before you start page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import { BeforeYouStart } from "./Components/BeforeYouStart/BeforeYouStart";
+import { Navbar } from "./components/Navbar";
+import { CareersCardExpanded } from "./components/CareersCardExpanded"
 
 function App() {
 	return (
-		<>
-		<BeforeYouStart></BeforeYouStart>
-		</>
+		<div className="bg-[#FFC273] h-[100vh]">
+			<Navbar />
+			<div className="flex justify-center">
+			<CareersCardExpanded
+				educationLevel={'high-school'}
+				category={'Agriculture and Natural Resources'}
+				careerName={'Agricultural Architect'}
+				title={"Agricultural Architect"} 
+				description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."}
+				salary={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."}
+				skills={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."}
+				colleges={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."}
+				majors={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."}
+
+				careerImages={['https://placehold.co/200x200', 'https://placehold.co/200x200', 'https://placehold.co/200x200','https://placehold.co/200x200', 'https://placehold.co/200x200', 'https://placehold.co/200x200' ]}
+				/>
+			</div>
+		</div>
 	);
 }
 
