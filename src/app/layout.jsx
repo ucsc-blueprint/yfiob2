e897438f57da865ecc964f76520f8867dc9f1476
Daniel Gonzalez <daniel.gonzalez10152@gmail.com>
2025-02-07 18:39:48 -0800

    fixed font

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,6 +4,12 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<head>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+				<link
+					href="https://fonts.googleapis.com/css2?family=Lato&display=swap"
+					rel="stylesheet"
+				/>
 				<meta charSet="UTF-8" />
 				<link rel="icon" href="../assets/YFIOBLogo.png" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 module.exports = {
 	content: [
 		"./src/app/**/*.{js,ts,jsx,tsx}",
-		"./src/app/*/*.{js,ts,jsx,tsx}",
 		"./src/pages/**/*.{js,ts,jsx,tsx}",
 		"./src/components/**/*.{js,ts,jsx,tsx}",
 	],
 
 	theme: {
+		fontFamily: {
+			lato: ["lato-regular", "Lato"],
+		},
 		extend: {},
 	},
 
