d75a0c7c0bd13c4fa8cf7df05e3aa7fd320f41e4
Brenda Aceves <blaceves@ucsc.edu>
2025-02-07 19:00:06 -0800

    added comments to code

--- src/Components/Shared/Button.jsx
+++ src/Components/Shared/Button.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 export const Button = ({text, size}) => {
-
+  // Button size is dependent on text-size
   if (size === "small") {
     return (
       <div className = "text-white font-lato font-normal text-[1.125rem] bg-[#4C78E7] w-max p-4 pl-8 pr-8 rounded-[30px]">{text}</div>
     );  
+  // Button size is based on parent container
   } else if (size === "big") {
     return (
       <div className = "text-white font-lato text-center w-full font-normal text-[1.125rem] bg-[#4C78E7] p-4 pl-8 pr-8 rounded-[30px]">{text}</div>
