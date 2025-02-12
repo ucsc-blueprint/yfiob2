b2ca117d4094a25f7573c60ac7d26e65057cdc36
Mandar Patil <mpatil2@ucsc.edu>
2025-02-11 19:03:57 -0800

    adding all files

--- src/app/take-quiz/page.jsx
+++ src/app/take-quiz/page.jsx
@@ -1,10 +1,28 @@
-import { Navbar } from "../../components/Navbar/Navbar";
+"use client"
 
-export default function page() {
+import { Navbar } from "../../components/Navbar/Navbar";
+import { QuestionCard } from "../../components/QuestionCard.jsx";
+import questions from "../../../questions.json";
+
+function getQuestions(educationLevel) {
+	const parsedData = Object.values(questions).map((element) => {
+		return element
+	})
+	
+	console.log(parsedData[educationLevel])
+	return parsedData[educationLevel]
+}
+
+export default function page({educationLevel}) {
 	return (
 		<>
+			{getQuestions(educationLevel).map((question) => {
+				console.log(question_id)
+				return <p>{question.question_id}</p>
+			})}
 			<Navbar />
 			<p>Take quiz page</p>
+			<QuestionCard />
 		</>
 	);
 }
