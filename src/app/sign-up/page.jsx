45abff96e50e1db8a72f9b3f18a69ac90f9bc06d
erwu6 <61030907+erwu6@users.noreply.github.com>
2025-04-02 21:40:22 -0700

    Testing out edited page

--- src/app/sign-up/page.jsx
+++ src/app/sign-up/page.jsx
@@ -1,10 +1,12 @@
 "use client";
 import { Navbar } from "../../components/Navbar/Navbar";
 import { useState, useEffect } from "react";
+import TextBox from "../../components/TextBox/TextBox.jsx";
+import Button from "../../components/Button.jsx";
 import addData from "../../utils/addData.js";
 import schoolData from "./schools.json";
 import gradeData from "./grades.json"; // Import grades.json
-import ArrowSVG from "../../assets/arrow.svg";
+import DropDownArrow from "../../components/DropDownArrow.jsx";
 
 export const Page = () => {
     const [email, setEmail] = useState("");
@@ -80,8 +82,8 @@ export const Page = () => {
     return (
         <>
             <Navbar />
-            <div className="bg-[#E8F6FF] min-h-[calc(100vh-64px)] flex items-center justify-center">
-                <div className="flex flex-col items-center justify-center">
+            <div className="bg-[#E8F6FF] flex flex-col justify-center items-center">
+                <div className = "space-y-4 w-[502px]">
                     <h1 className="text-4xl font-sofia font-medium text-[40px] mb-8 leading-[40px] tracking-normal text-center">
                         Sign up
                     </h1>
@@ -90,90 +92,55 @@ export const Page = () => {
                     <p className="text-left m-3 w-[502px] font-lato font-normal text-[20px] leading-[20px] tracking-normal">
                         Email
                     </p>
-                    <input
-                        placeholder="Email"
-                        onChange={(e) => setEmail(e.target.value)}
-                        value={email}
-                        id="email"
-                        onFocus={(e) => e.target.style.color = "black"}  // Change text color to black on focus
-                        onBlur={(e) => e.target.style.color = "#898989"}
-                        className="w-full h-[61px] placeholder-gray-400 text-gray-900 focus:text-black focus:outline-none"
-                        style={{width: "502px",
-                          height: "61px",
-                          borderRadius: "10px",
-                          textIndent: '30px',
-                          fontFamily: "Lato",
-                          color: "#898989",
-                          fontWeight: 400,
-                          lineHeight: '20px',
-                          fontSize: '20px',
-                              }}
+                    <TextBox 
+                            type = "text"
+                            Placeholder = {"Email"}
+                            onChange={(e) => setEmail(e.target.value)}
+                            value={email}
+                            id="email"
+                            onFocus={(e) => e.target.style.color = "black"}  // Change text color to black on focus
+                            onBlur={(e) => e.target.style.color = "#898989"}
                     />
 
                     {/* Password Input */}
                     <p className="text-left m-3 w-[502px] font-lato font-normal text-[20px] leading-[20px] tracking-normal">
                         Password
                     </p>
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        onChange={(e) => setPassword(e.target.value)}
-                        value={password}
-                        id="password"
-                        onFocus={(e) => e.target.style.color = "black"}  // Change text color to black on focus
-                        onBlur={(e) => e.target.style.color = "#898989"}
-                        className="focus:outline-none"
-                        style={{width: "502px",
-                            height: "61px",
-                            borderRadius: "10px",
-                            textIndent: '30px',
-                            fontFamily: "Lato",
-                            color: "#898989",
-                            fontWeight: 400,
-                            lineHeight: '20px',
-                            fontSize: '20px',
-                              }}
+                    <TextBox 
+                            type="password"
+                            Placeholder="Password"
+                            onChange={(e) => setPassword(e.target.value)}
+                            value={password}
+                            id="password"
+                            onFocus={(e) => e.target.style.color = "black"}  // Change text color to black on focus
+                            onBlur={(e) => e.target.style.color = "#898989"}
                     />
 
                     <p className="text-left mt-3 w-[502px] font-lato font-normal text-[20px] leading-[20px] tracking-normal">
                         School Information
                     </p>
 
-                    {/* //TODO Add the arrows to the dropdowns */}
                     {/* School Dropdown Input */}
-                    <div className="relative w-[502px] m-3">
-                        <input
-                            type="text"
-                            placeholder="School"
-                            value={searchTerm}
-                            onChange={(e) => {
-                                setSearchTerm(e.target.value);
-                                setSchool(e.target.value);
-                            }}
-                            onFocus={(e) => {
-                              e.target.style.color = "black"; 
-                              setIsDropdownVisible(true);
-                            }}
-                            onBlur={(e) => {
-                              setTimeout(() => setIsDropdownVisible(false), 200);
-                              e.target.style.color = "#898989";
-                          }}
-                            className="dropdown text-left w-full h-[61px] rounded-lg bg-white focus:outline-none"
-                            style={{
-                                textAlign: "left",
-                                textIndent: "30px",
-                                fontFamily: "Lato",
-                                color: "#898989",
-                                fontWeight: 400,
-                                lineHeight: "20px",
-                                fontSize: "20px",
-                            }}
-                        />
+                    <div className="relative">
+                    <TextBox 
+                                Placeholder="School"
+                                onChange={(e) => {
+                                    setSearchTerm(e.target.value);
+                                    setSchool(e.target.value);
+                                }}
+                                value={searchTerm}
+                                onFocus={(e) => {
+                                    e.target.style.color = "black"; 
+                                    setIsDropdownVisible(true);
+                                }}
+                                onBlur={(e) => {
+                                    setTimeout(() => setIsDropdownVisible(false), 200);
+                                    e.target.style.color = "#898989";
+                                }}
+                            />
                        
                        {/* Arrow SVG */}
-                        <svg width="21" height="13" viewBox="0 0 21 13" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute right-3 top-1/2 w-6 h-6 -translate-y-1/2 pointer-events-none">
-                        <path d="M8.21183 12.1522L0.864005 4.80435C-0.0309225 3.90942 -0.230632 2.88543 0.264875 1.73239C0.758498 0.577464 1.64118 0 2.91292 0H17.4673C18.739 0 19.6217 0.577464 20.1153 1.73239C20.6108 2.88543 20.4111 3.90942 19.5162 4.80435L12.1684 12.1522C11.8857 12.4348 11.5796 12.6467 11.2499 12.788C10.9202 12.9293 10.5669 13 10.1901 13C9.81328 13 9.46002 12.9293 9.13031 12.788C8.8006 12.6467 8.49444 12.4348 8.21183 12.1522Z" fill="#4C78E7" fill-opacity="0.5"/>
-                        </svg>
+                        <DropDownArrow/>
 
                         {/* Dropdown List */}
                         {isDropdownVisible && (
@@ -200,33 +167,20 @@ export const Page = () => {
                     </div>
 
                     {/* Grade Dropdown Input */}
-                    <div className="relative w-[502px] mb-3">
-                        <input
-                            type="text"
-                            placeholder="Grade"
-                            value={gradeSearchTerm}
-                            onChange={(e) => {
-                                setGradeSearchTerm(e.target.value);
-                                setGrade(e.target.value);
-                            }}
-                            onFocus={() => setIsGradeDropdownVisible(true)}
-                            onBlur={() => setTimeout(() => setIsGradeDropdownVisible(false), 200)}
-                            className="dropdown text-left w-full h-[61px] rounded-lg bg-white"
-                            style={{
-                                textAlign: "left",
-                                textIndent: "30px",
-                                fontFamily: "Lato",
-                                color: "#898989",
-                                fontWeight: 400,
-                                lineHeight: "20px",
-                                fontSize: "20px",
-                            }}
-                        />
+                    <div className="relative">
+                    <TextBox 
+                                Placeholder="Grade"
+                                onChange={(e) => {
+                                    setGradeSearchTerm(e.target.value);
+                                    setGrade(e.target.value);
+                                }}
+                                value={gradeSearchTerm}
+                                onFocus={() => setIsGradeDropdownVisible(true)}
+                                onBlur={() => setTimeout(() => setIsGradeDropdownVisible(false), 200)}
+                            />
 
                         {/* Arrow SVG */}
-                        <svg width="21" height="13" viewBox="0 0 21 13" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute right-3 top-1/2 w-6 h-6 -translate-y-1/2 pointer-events-none">
-                        <path d="M8.21183 12.1522L0.864005 4.80435C-0.0309225 3.90942 -0.230632 2.88543 0.264875 1.73239C0.758498 0.577464 1.64118 0 2.91292 0H17.4673C18.739 0 19.6217 0.577464 20.1153 1.73239C20.6108 2.88543 20.4111 3.90942 19.5162 4.80435L12.1684 12.1522C11.8857 12.4348 11.5796 12.6467 11.2499 12.788C10.9202 12.9293 10.5669 13 10.1901 13C9.81328 13 9.46002 12.9293 9.13031 12.788C8.8006 12.6467 8.49444 12.4348 8.21183 12.1522Z" fill="#4C78E7" fill-opacity="0.5"/>
-                        </svg>
+                        <DropDownArrow/>
 
                         {/* Dropdown List */}
                         {isGradeDropdownVisible && (
@@ -253,30 +207,22 @@ export const Page = () => {
                     </div>
 
                     {/* Student ID Input */}
-                        <input
-                            placeholder="Student ID Number"
+                    <TextBox 
+                            Placeholder="Student ID Number"
                             onChange={(e) => setStudentID(e.target.value)}
                             value={studentID}
-
-                            style={{width: "502px",
-                                height: "61px",
-                                borderRadius: "10px",
-                                textIndent: '30px',
-                                fontFamily: "Lato",
-                                color: "#898989",
-                                fontWeight: 400,
-                                lineHeight: '20px',
-                                fontSize: '20px',
-                                }}
+                            onFocus={(e) => e.target.style.color = "black"}  // Change text color to black on focus
+                            onBlur={(e) => e.target.style.color = "#898989"}
                         />
-
-                    <button
-                        onClick={handleSubmit}
-                        className="w-[502px] h-[58px] gap-[10px] rounded-[30px] px-[213px] py-[17px] bg-[#4C78E7] text-white flex items-center justify-center mt-9"
-                    >
-                        Sign Up
-                    </button>
-                </div>
+                    </div>
+                    
+                    <div className="my-6 w-[502px]">
+                        <Button
+                            text = "Sign Up"
+                            size = "big"
+                            onClick = {handleSubmit}
+                        />
+                    </div>
             </div>
         </>
     );
