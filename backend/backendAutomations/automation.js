85c376975b56768fa6a31548901eec18fd9172aa
Akshay Kamath <akkamath@ucsc.edu>
2025-04-14 16:17:02 -0700

    adding matching algorithm

---
+++ backend/backendAutomations/automation.js
@@ -0,0 +1,28 @@
+import { Careers } from "../../constants/CAREERS.js";
+
+import { db } from "../firebaseConfig.js";
+import { addDoc, collection } from "firebase/firestore";
+
+async function operate(tableName, industryAttribute) {
+    const ref = collection(db, tableName);
+    for(const industry in Careers){
+        const arr = Careers[industry][industryAttribute];
+
+        for(const val of arr){ 
+            //console.log(industry + " = " + val);
+            
+            
+            addDoc(ref, {"industry": industry, [industryAttribute]: val}).then(res => {
+                console.log(res);
+            }).catch(err => {
+                console.log(err);
+            })
+            
+            
+        }
+    }
+}
+
+const tableName = "companies"
+const tableAttributeCareersJSON = "companies"
+operate(tableName, tableAttributeCareersJSON)
