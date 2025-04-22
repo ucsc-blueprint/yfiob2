932647de310dc8f823d2014d2d18fe746bf0fa3d
Mandar Patil <mpatil2@ucsc.edu>
2025-04-21 18:32:37 -0700

    adding all files

--- src/components/QuestionCard/QuestionCard.jsx
+++ src/components/QuestionCard/QuestionCard.jsx
@@ -14,15 +14,37 @@ export const QuestionCard = ({
 	questionNumber,
 	totalQuestions,
 	selectedAnswer, 
+	grade, 
 	onAnswerSelect = () => {},
 }) => {
-	const [clickedButton, setClickedButton] = useState(null);
 
+
+	const backgroundColor = {
+		"elementary-school": "bg-[#3FA1D9]",
+		"middle-school": "bg-[#47B748]",
+		"high-school":  "bg-[#FF7022]", 
+	}
+
+	const textColor = {
+		"elementary-school": "text-[#3FA1D9]", 
+		"middle-school": "text-[#47B748]",
+		"high-school":  "text-[#FF7022]", 
+	}
+
+	const puzzleColor = {
+		"elementary-school": "/jigna-small-blue.svg",
+		"middle-school": "/jigna-small-green.svg",
+		"high-school":  "/jigna-small-orange.svg", 
+	}
+	
+	const bgColor = backgroundColor[grade]
+	const tColor = textColor[grade]
+	const puzzleImg = puzzleColor[grade]
 
 	return (
-		<div className="bg-slate-400 w-[66vw] h-[50vh] rounded-[20px] font-lato shadow-md overflow-hidden">
+		<div className={`bg-slate-300 w-[66vw] h-[50vh] rounded-[20px] font-lato shadow-md overflow-hidden`}>
 			<div
-				className="bg-green-500 h-[10%] w-[25%] rounded-tl-[20px]"
+				className={`${bgColor} h-[10%] w-[25%] rounded-tl-[20px]`}
 				style={{ width: `${Math.floor((questionNumber / totalQuestions) * 100)}%` }}
 				
 			></div>
@@ -31,7 +53,7 @@ export const QuestionCard = ({
 					<p className="flex-[1] w-[77%] text-[114%] text-center italic">{advice}</p>
 					<div className="flex-[3] w-[77%] flex justify-between">
 						<h1 className="text-3xl font-bold">"{question}"</h1>
-						<img width={"20%"} src="/jigna-small.svg" alt="Jigna Small" />
+						<img width={"20%"} src={`${puzzleImg}`} alt="Jigna Small" />
 					</div>
 					<div className="flex-[1] w-[75%] flex-row flex justify-center pb-4 ">
 						{agreeArray.map((agreement, i) => {
@@ -40,8 +62,8 @@ export const QuestionCard = ({
 								<button
 									key={i}
 									onClick={() => onAnswerSelect(i)}
-									className={`flex-[${weight}] text-center text-slate-500 m-[1%] rounded-[20px] px-4 py-3 ${
-										selectedAnswer === i ? 'bg-green-500 text-white' : 'bg-slate-200'
+									className={`flex-[${weight}] text-center m-[1%] rounded-[20px] px-4 py-3 ${
+										selectedAnswer === i ? `${bgColor} text-white` : 'bg-slate-200 text-slate-500' 
 									}`}
 								>
 									{agreement}
@@ -51,7 +73,7 @@ export const QuestionCard = ({
 					</div>
 				</div>
 				<div className="flex justify-end items-end text-2xl pb-[2vh]">
-					<span className="text-green-600">{questionNumber}</span>
+					<span className={`${tColor}`}>{questionNumber}</span>
 					<span className="text-slate-400">/{totalQuestions}</span> 
 				</div>
 			</div>
