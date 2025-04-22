e8838991263633777ab66e0ae388d4200fffd4f1
Mandar Patil <32528097+mandp711@users.noreply.github.com>
2025-04-21 18:07:07 -0700

    Revert "adding question_classification"

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,26 +1,18 @@
-"use client";
-
 import React from "react";
 import { Navbar } from "../components/Navbar/Navbar";
-import { useRouter } from "next/navigation";
 
 function App() {
-  const router = useRouter();
-
-  const handleGradeClick = (grade) => {
-    router.push(`/take-quiz/${grade}`);
-  };
-
   return (
     <div className="min-h-screen">
       <Navbar />
-      {/* Top Section */}
+      
+      {/* Top half-blue section */}
       <div className="bg-[#E8F6FF] h-[40vh] flex flex-col items-center justify-center">
         <h1 className="text-3xl font-kumbh mb-4">
           Discover a career path that matches your unique interests!
         </h1>
         <div className="flex items-center justify-center space-x-40 mt-6">
-        <img
+          <img
             src="/assets/ConstructionPuzzlePiece.svg"
             alt="K-5 Puzzle Piece"
             className="w-36 h-36"
@@ -48,21 +40,33 @@ function App() {
         </div>
       </div>
 
-      {/* Career Quiz Section */}
+      {/* Rest of content */}
       <div className="max-w-7xl mx-auto py-10 px-4">
         <div className="text-center mb-8">
           <h2 className="text-2xl font-kumbh">Career Quiz</h2>
           <p className="mt-2 font-kumbh">Choose your grade level to begin!</p>
         </div>
         <div className="flex items-center justify-center space-x-8">
-          <button onClick={() => handleGradeClick('elementary-school')} className="flex flex-col items-center cursor-pointer focus:outline-none">
-            <img src="/assets/K-5PuzzlePiece.svg" alt="K-5 Puzzle Piece" className="w-[150px] h-[150px]" />
+          <button className="flex flex-col items-center cursor-pointer focus:outline-none">
+            <img
+              src="/assets/K-5PuzzlePiece.svg"
+              alt="K-5 Puzzle Piece"
+              className="w-[150px] h-[150px]"
+            />
           </button>
-          <button onClick={() => handleGradeClick('middle-school')} className="flex flex-col items-center cursor-pointer focus:outline-none">
-            <img src="/assets/6-8PuzzlePiece.svg" alt="6-8 Puzzle Piece" className="w-[250px] h-[150px]" />
+          <button className="flex flex-col items-center cursor-pointer focus:outline-none">
+            <img
+              src="/assets/6-8PuzzlePiece.svg"
+              alt="6-8 Puzzle Piece"
+              className="w-[250px] h-[150px]"
+            />
           </button>
-          <button onClick={() => handleGradeClick('high-school')} className="flex flex-col items-center cursor-pointer focus:outline-none">
-            <img src="/assets/9-12PuzzlePiece.svg" alt="9-12 Puzzle Piece" className="w-[150px] h-[150px]" />
+          <button className="flex flex-col items-center cursor-pointer focus:outline-none">
+            <img
+              src="/assets/9-12PuzzlePiece.svg"
+              alt="9-12 Puzzle Piece"
+              className="w-[150px] h-[150px]"
+            />
           </button>
         </div>
       </div>
