b5cb955ad9a29abfc8cd0bbadbaa3d120fb63cab
Daniel Gonzalez <daniel.gonzalez10152@gmail.com>
2025-04-24 20:20:23 -0700

    added college popup

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,27 +1,14 @@
 import React from "react";
 import { Navbar } from "../components/Navbar/Navbar";
-import { CareersCardExpanded } from "../components/CareersCardExpanded/CareersCardExpanded";
+import { CollegeCard } from "../components/CollegeCard";
 
 function App() {
-    const defaultCardProps = {
-        educationLevel: "high-school",
-        category: "Technology",
-        careerName: "Software Developer",
-        description: "Develops software applications and systems",
-        salary: "$80,000 - $150,000",
-        skills: ["computer science", "software engineering", "information technology"],
-        careerImages: [
-            "https://images.unsplash.com/photo-1657343585287-b5c42ff0d0e3?w=1200&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXxPemNMV2dLQXRXVXx8ZW58MHx8fHx8",
-            "https://images.unsplash.com/photo-1696221734265-d2020cffa479?w=1200&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NXxPemNMV2dLQXRXVXx8ZW58MHx8fHx8",
-        ],
-        colleges: ["ucsc", "stanford", "mit"],
-        majors: "Computer Science, Software Engineering",
-    };
-
     return (
         <div className="min-h-screen">
             <Navbar />
-            <CareersCardExpanded {...defaultCardProps} />
+            <div className="flex justify-center items-center m-16">
+                <CollegeCard />
+            </div>
         </div>
     );
 }
---
+++ src/components/CollegeCard.jsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+import UniversityCampus from "./UniversityCampus";
+
+const agreeArray = ["Yes", "Maybe", "No"];
+
+function Button(props) {
+    return (
+        <div className="flex grow justify-center" {...props}>
+            <button
+                className={
+                    "text-center bg-slate-200 text-slate-500 w-28 rounded-[20px] px-4 py-3"
+                }
+            >
+                {props.children}
+            </button>
+        </div>
+    );
+}
+
+export const CollegeCard = ({ onAnswerSelect = () => {} }) => {
+    return (
+        <div className="bg-slate-400 w-[66vw] h-[50vh] rounded-[20px] font-lato shadow-md overflow-hidden">
+            <div className="bg-white h-full rounded-b-[20px] p-[5vh] flex">
+                {/* Images */}
+                <div className="w-2/5 h-full flex flex-col">
+                    <div className="grow relative mb-1 rounded-md overflow-hidden">
+                        <Image
+                            fill
+                            className="object-cover"
+                            src={"https://placehold.co/500x300"}
+                            alt="College"
+                        />
+                    </div>
+                    <div className="grow relative mt-1 rounded-md overflow-hidden">
+                        <Image
+                            fill
+                            className="object-cover"
+                            src={"https://placehold.co/500x300"}
+                            alt="College"
+                        />
+                    </div>
+                </div>
+                <div className="w-3/4 flex justify-center items-center">
+                    <div className="flex w-3/4 h-3/4 flex-col items-center justify-between">
+                        <h1 className="font-bold text-3xl text-center">
+                            Are you interested in attending college?
+                        </h1>
+                        <div>
+                            <UniversityCampus />
+                        </div>
+                        <div className="flex w-full items-stretch ">
+                            {agreeArray.map((agree) => (
+                                <Button>{agree}</Button>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
