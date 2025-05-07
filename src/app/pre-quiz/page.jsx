f4e4bab0244d7dbeae3dc6a35fbe4990ee6d2ff8
erwu6 <61030907+erwu6@users.noreply.github.com>
2025-05-07 14:15:18 -0700

    Added const handleGradeClick

--- src/app/pre-quiz/page.jsx
+++ src/app/pre-quiz/page.jsx
@@ -1,8 +1,26 @@
+"use client";
+
 import React from "react";
 import { Navbar } from "../../components/Navbar/Navbar";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { auth } from "../../utils/firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 function App() {
+  const router = useRouter();
+
+  const handleGradeClick = (grade) => {
+    const userAuth = auth;
+
+    onAuthStateChanged(userAuth, (user) => {
+        if (user) {
+            router.push(`/take-quiz/${grade}?valid=true`);
+        } else {
+            router.push(`/choose-account-type?=${grade}`);
+        }
+    });
+};
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="z-10 relative">
@@ -17,33 +35,36 @@ function App() {
             <p className="mt-3 font-kumbh text-lg">Choose your grade level to begin!</p>
           </div>
           <div className="flex items-center justify-center space-x-12">
-            <Link href="/take-quiz/elementary-school">
-              <button className="flex flex-col items-center cursor-pointer focus:outline-none transform hover:scale-105 transition-transform">
-                <img
-                  src="/assets/K-5PuzzlePiece.svg"
-                  alt="K-5 Puzzle Piece"
-                  className="w-[180px] h-[180px]"
-                />
-              </button>
-            </Link>
-            <Link href="/take-quiz/middle-school">
-              <button className="flex flex-col items-center cursor-pointer focus:outline-none transform hover:scale-105 transition-transform">
-                <img
-                  src="/assets/6-8PuzzlePiece.svg"
-                  alt="6-8 Puzzle Piece"
-                  className="w-[300px] h-[180px]"
-                />
-              </button>
-            </Link>
-            <Link href="/take-quiz/high-school">
-              <button className="flex flex-col items-center cursor-pointer focus:outline-none transform hover:scale-105 transition-transform">
-                <img
-                  src="/assets/9-12PuzzlePiece.svg"
-                  alt="9-12 Puzzle Piece"
-                  className="w-[180px] h-[180px]"
-                />
-              </button>
-            </Link>
+            <button
+              onClick={() => handleGradeClick("elementary-school")}
+              className="flex flex-col items-center cursor-pointer focus:outline-none"
+            >
+              <img
+                src="/assets/K-5PuzzlePiece.svg"
+                alt="K-5 Puzzle Piece"
+                className="w-[180px] h-[180px]"
+              />
+            </button>
+            <button
+              onClick={() => handleGradeClick("high-school")}
+              className="flex flex-col items-center cursor-pointer focus:outline-none"
+            >
+              <img
+                src="/assets/6-8PuzzlePiece.svg"
+                alt="6-8 Puzzle Piece"
+                className="w-[300px] h-[180px]"
+              />
+            </button>
+            <button
+              onClick={() => handleGradeClick("high-school")}
+              className="flex flex-col items-center cursor-pointer focus:outline-none"
+            >
+              <img
+                src="/assets/9-12PuzzlePiece.svg"
+                alt="9-12 Puzzle Piece"
+                className="w-[180px] h-[180px]"
+              />
+            </button>
           </div>
         </div>
       </div>
