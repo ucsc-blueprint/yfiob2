c6824188d4472a4055350e14f604a2b97f8b1492
Akshay Kamath <akkamath@ucsc.edu>
2025-08-28 16:28:51 -0700

    moving everything to the client and ensuring firebase is only a fallback in case localstorage was cleared

--- backend/matchingAlgorithm/matchingAlgo.js
+++ backend/matchingAlgorithm/matchingAlgo.js
@@ -24,48 +24,7 @@ algorithm:
 //import { db } from "../firebase.js";
 import {db} from "../../src/utils/firebase.js";
 import { collection, getDocs, query, where, addDoc, deleteDoc, orderBy, serverTimestamp} from "firebase/firestore";
-
-
-const allQuestions = {
-  '0': 'physical',
-  '1': 'observe',
-  '2': 'creative',
-  '3': 'teach',
-  '4': 'lead',
-  '5': 'engineer'
-}
-const allClassifications = {
-  'physical|5': 'Building and Construction Trades',
-  'creative|3': 'Hospitality, Tourism, and Recreation',
-  'teach|1': 'Energy, Environment, and Utilities',
-  'teach|5': 'Hospitality, Tourism, and Recreation',
-  'physical|4': 'Fashion and Interior Design',
-  'creative|5': 'Fashion and Interior Design',
-  'lead|5': 'Manufacturing and Product Development',
-  'physical|3': 'Energy, Environment, and Utilities',
-  'engineer|3': 'Agriculture and Natural Resources',
-  'observe|4': 'Agriculture and Natural Resources',
-  'teach|4': 'Business and Finance',
-  'lead|1': 'Agriculture and Natural Resources',
-  'observe|3': 'Health Science and Medical Technology',
-  'lead|3': 'Fashion and Interior Design',
-  'physical|1': 'Information and Communication Technology',
-  'physical|2': 'Hospitatility, Tourism, and Recreation',
-  'engineer|2': 'Information and Communication Technology',
-  'creative|1': 'Building and Construction Trades',
-  'teach|3': 'Building and Construction Trades',
-  'observe|1': 'Information and Communication Technology',
-  'engineer|1': 'Arts, Media and Entertainment',
-  'creative|2': 'Agriculture and Natural Resources',
-  'teach|2': 'Public Services',
-  'engineer|5': 'Engineering and Architecture',
-  'engineer|4': 'Marketing, Sales, and Service',
-  'lead|4': 'Arts, Media, and Entertainment',
-  'observe|2': 'Arts, Media and Entertainment',
-  'creative|4': 'Marketing, Sales, and Service',
-  'lead|2': 'Health Science and Medical Technology',
-  'observe|5': 'Engineering and Architecture'
-}
+import { CAREERS, ALL_QUESTIONS, ALL_CLASSIFICATIONS } from "./constants.js";
 
 const loadFromLocalStorage = () => {
     const storedAnswers = localStorage.getItem("answers");
@@ -145,10 +104,10 @@ export default async function storeTopKIndustries(username, k, grade = "", colle
 
     for (let questionNumber = 0; questionNumber < responses.length; questionNumber++) {
         const optionSelected = (responses[questionNumber] + 1).toString();
-        const category = allQuestions[questionNumber];
+        const category = ALL_QUESTIONS[questionNumber];
 
         const industryKey = `${category}|${optionSelected}`;
-        const industry = allClassifications[industryKey];
+        const industry = ALL_CLASSIFICATIONS[industryKey];
 
         if (industry in industries) {
             industries[industry] += 1;
@@ -178,7 +137,7 @@ export default async function storeTopKIndustries(username, k, grade = "", colle
                 })
             );
 
-            localIndustries.push({
+            localIndustries.unshift({
                 industry: arr[i][0],
                 ranking: i,
                 percentage: Number(((arr[i][1] / totalTopIndustryCount) * 100).toFixed(2))
@@ -220,13 +179,5 @@ export async function getTopKIndustries(username){
 }
 
 export async function getCareersForIndustry(industry){
-    const careersFoundQuery = query(collection(db, "careers"), where("industry", "==", industry));
-    const careersFound = await getDocs(careersFoundQuery);
-    const careers = [];
-
-    for (const doc of careersFound.docs) {
-        const data = doc.data();
-        careers.push(data.careers);
-    }
-    return careers;
+    return CAREERS[industry] || [];
 }
